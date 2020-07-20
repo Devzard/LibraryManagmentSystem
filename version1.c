212bf6a9fb8f1252eb5831ca1f90a886c3d6f47e
Drun-sites <debashishcoc@gmail.com>
2020-07-20 15:15:38 +0530

    delete added

--- version1.c
+++ version1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void concatString(char *mainString, char *target)
 {
@@ -32,9 +33,12 @@ void newBookEntry()
     char s[2];
     fgets(s, sizeof(s), stdin);
 
+    //clear the console
+    printf("\e[1;1H\e[2J");
+
     //ask for details
     //using fgets instead of gets as fgets is safe to use in comparison to gets since it checks for character array str bounds
-    printf("\n\tEnter the name of the book (max : 100) : ");
+    printf("\n\tEnter the name of the book (max : 20) : ");
     fgets(bookName, sizeof(bookName), stdin);
     concatString(finalInputValue, bookName);
 
@@ -90,6 +94,7 @@ void displayBooks()
 
     FILE *filePointer;
 
+    int serialNumber = 0;
     char readLine[1000];
 
     //open the file in read mode
@@ -99,15 +104,23 @@ void displayBooks()
     printf("\e[1;1H\e[2J");
 
     if (filePointer == NULL)
-        printf("\n\nFile not present.\n");
+        printf("\n\n***File not present.***\n");
     else
     {
-        printf("\n\nFile opened.\n\n");
+        printf("\n\n***File opened.***\n\n");
 
         while (fgets(readLine, 1000, filePointer) != NULL)
         {
             int wordCount = 0;
             // printf("\t%s", readLine);
+
+            //printing the serial number
+            if (serialNumber != 0)
+                printf("%d", serialNumber);
+            else
+                printf("Sl no.");
+            serialNumber++;
+
             printf("\t");
 
             for (int i = 0; i < strlen(readLine); i++)
@@ -130,6 +143,83 @@ void displayBooks()
     }
 
     printf("\n***End of file***\n");
+}
+
+// function to copy files that take name of the file to be copied and to the file to be copied
+void copyFiles(char firstFile[], char secondFile[])
+{
+    FILE *firstFilePointer, *secondFilePointer;
+
+    char readLine[1000];
+
+    firstFilePointer = fopen(firstFile, "r");
+    secondFilePointer = fopen(secondFile, "w");
+
+    if (firstFilePointer == NULL)
+        printf("\nFile to be copied can't be opened\n");
+    else
+    {
+        while (fgets(readLine, 1000, firstFilePointer) != NULL)
+        {
+            fputs(readLine, secondFilePointer);
+        }
+        fclose(firstFilePointer);
+        fclose(secondFilePointer);
+    }
+}
+
+//deleting a book
+void deleteBook()
+{
+    printf("\n************************************");
+
+    FILE *filePointer, *tempFilePointer;
+
+    char readLine[1000];
+    int deleteRecordSerialNumber = 0, serialNumber = 0;
+
+    filePointer = fopen("BookData.csv", "r");
+    tempFilePointer = fopen("TempFile.csv", "w");
+
+    // ask for the record to be deleted
+    printf("\nEnter the serial number of the record to be deleted : ");
+    scanf("%d", &deleteRecordSerialNumber);
+    if (deleteRecordSerialNumber <= 0)
+    {
+        printf("\n***Invalid Input***\n");
+        exit(0);
+    }
+
+    //clear the console
+    printf("\e[1;1H\e[2J");
+
+    if (filePointer == NULL)
+        printf("\n\n***File not present.***\n");
+    else
+    {
+        //write all the data in a temp file without the deleted data
+        while (fgets(readLine, 1000, filePointer) != NULL)
+        {
+            if (serialNumber != deleteRecordSerialNumber)
+            {
+                fputs(readLine, tempFilePointer);
+            }
+            serialNumber++;
+        }
+
+        if (deleteRecordSerialNumber > serialNumber)
+            printf("\n***Invalid Input***\n");
+        fclose(filePointer);
+        fclose(tempFilePointer);
+    }
+    copyFiles("TempFile.csv", "BookData.csv");
+
+    //delete the temp file
+    int del = remove("TempFile.csv");
+    if (del != 0)
+        printf("\n***Something went wrong***\n");
+
+    printf("\n\nSuccessfully Deleted.\n\n");
 }
 
 int main()
@@ -181,6 +271,8 @@ int main()
             break;
         case '2':
             printf("\n Option 2 selected.");
+            displayBooks();
+            deleteBook();
             break;
         case '3':
             printf("\n Option 3 selected.");
